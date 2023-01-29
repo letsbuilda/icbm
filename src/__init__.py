9fd0cc54ec5fc53ce416271e43cb70aab10aee89
SomeHybrid <91648368+SomeHybrid@users.noreply.github.com>
2023-01-29 08:55:48 +0000

    init

---
+++ src/__init__.py
@@ -0,0 +1,22 @@
+"""
+An intercontinental ballistic missile.
+
+Because why the hell not.
+"""
+
+__version__ = "0.0.1"
+
+print("""
+         /\\
+        /  \\
+        |  |
+        |  |
+       / == \ 
+       |/**\|
+
+        ICBM
+  Intercontinental
+ Ballistic Missile
+
+ In construction
+""")
---
+++ tests/test_icbm.py
@@ -0,0 +1,4 @@
+import pytest
+
+def tests():
+    assert __version__ == "0.0.0"
